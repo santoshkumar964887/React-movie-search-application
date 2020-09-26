41bc937cd24ca27b5903daeafad1612d1165714f
Santosh Kumar <santoshkumar964887@gmail.com>
2020-09-26 19:30:51 +0530

    designing card as flex

--- src/components/ResultCard.js
+++ src/components/ResultCard.js
@@ -15,32 +15,39 @@ export const ResultCard = ({ movie }) => {
 
   return (
     <div className="result-card">
-      <div className="poster-wrappers">
-        {movie.Poster ? (
-          <img className="poster1" src={movie.Poster} alt={`${movie.Title} Poster`} />
-        ) : (
-          <div className="filler-poster" />
-        )}
-      </div>
-
-      {/* <div className="info">
-        <div className="header">
+      <div className="card-container1">
+        <div>
           <h3 className="title">{movie.Title}</h3>
-          <h4 className="release-date">
-            <Moment format="YYYY">{movie.Year}</Moment>
-          </h4>
+        </div>
+        <div className="poster-wrappers">
+          {movie.Poster ? (
+            <img
+              className="poster1"
+              src={movie.Poster}
+              alt={`${movie.Title} Poster`}
+            />
+          ) : (
+            <div className="filler-poster" />
+          )}
         </div>
 
-        <div className="controls">
-          <button
-            className="btn"
-            disabled={watchedDisabled}
-            onClick={() => addMovieToWatched(movie)}
-          >
-            Favorite
-          </button>
+        <div className="info1">
+          <div><h4>Release In</h4></div>
+          <div><h4 className="release-date">
+            <Moment format="YYYY">{movie.Year}</Moment>
+          </h4></div>
+          
         </div>
-      </div> */}
+      </div>
+      <div className="controls">
+        <button
+          className="btn"
+          disabled={watchedDisabled}
+          onClick={() => addMovieToWatched(movie)}
+        >
+          Favorite
+        </button>
+      </div>
     </div>
   );
 };
