3299805cee84e81df0f9e89e87a333394c6e92a3
Santosh Kumar <santoshkumar964887@gmail.com>
2020-09-27 09:50:00 +0530

    Updates

---
+++ precache-manifest.bacc88616e953e6ea25b3e9935a71199.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "394cab12def02661dffaa2eb35b80b03",
+    "url": "/React-movie-search-application/index.html"
+  },
+  {
+    "revision": "d017dc608efe35ae0566",
+    "url": "/React-movie-search-application/static/css/main.5c6f9336.chunk.css"
+  },
+  {
+    "revision": "d8150aba388842c958b3",
+    "url": "/React-movie-search-application/static/js/2.f4c95f2f.chunk.js"
+  },
+  {
+    "revision": "228bac5b7587832345b091357f4a52ce",
+    "url": "/React-movie-search-application/static/js/2.f4c95f2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d017dc608efe35ae0566",
+    "url": "/React-movie-search-application/static/js/main.9739434d.chunk.js"
+  },
+  {
+    "revision": "966407333832334990e4",
+    "url": "/React-movie-search-application/static/js/runtime-main.3d892414.js"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/React-movie-search-application/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "3debcaf1a98e4445d213fd0089d42f6b",
+    "url": "/React-movie-search-application/static/media/fa-brands-400.3debcaf1.svg"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/React-movie-search-application/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/React-movie-search-application/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/React-movie-search-application/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/React-movie-search-application/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89f9806b964f92ad282fd60947eba588",
+    "url": "/React-movie-search-application/static/media/fa-regular-400.89f9806b.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/React-movie-search-application/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/React-movie-search-application/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/React-movie-search-application/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/React-movie-search-application/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/React-movie-search-application/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "ad912fd102f4052d3273dd838e0c671d",
+    "url": "/React-movie-search-application/static/media/fa-solid-900.ad912fd1.svg"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/React-movie-search-application/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/React-movie-search-application/static/media/fa-solid-900.bea989e8.woff"
+  }
+]);
